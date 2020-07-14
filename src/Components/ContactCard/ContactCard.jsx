e21cde3733f8ff5dfecb6e0c909a0c54b95faaf5
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-07-14 18:20:45 +0430

    Modify UI

--- src/Components/ContactCard/ContactCard.jsx
+++ src/Components/ContactCard/ContactCard.jsx
@@ -8,7 +8,6 @@ class ContactCard extends Component {
         <img className="ContactCard__image" src={"https://api.adorable.io/avatars/150/" + Math.random()} alt="profile" />
         <span className="ContactCard__title">Abolfazl</span>
         <span className="ContactCard__number">09220382257</span>
-        <span className="ContactCard__email">abolfazlsdfsahiazar@gmail.com</span>
       </div>
     );
   }
