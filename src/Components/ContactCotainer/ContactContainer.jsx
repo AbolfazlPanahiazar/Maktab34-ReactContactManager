cfc5b32ad9396d494204af28113874cb54daeb06
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-07-15 15:41:06 +0430

    Add search input effect

--- src/Components/ContactCotainer/ContactContainer.jsx
+++ src/Components/ContactCotainer/ContactContainer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CotanctContainerHeder from "../CotanctContainerHeder/CotanctContainerHeder";
 import ContactContainerBody from "../ContactContainerBody/ContactContainerBody";
+import "./ContactContainer.scss";
 
 class ContactContainer extends Component {
   constructor() {
@@ -12,8 +13,14 @@ class ContactContainer extends Component {
         { id: "v57je", name: "Calum Scott", phone: "9512548-89" },
         { id: "qw73c", name: "natalie Portman", phone: "036521/542" },
       ],
+      searchInput: "",
     };
   }
+
+  searchInputHandler = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -24,10 +31,17 @@ class ContactContainer extends Component {
   }
 
   render() {
+    const contacts = this.state.contacts.filter((contact) => contact.name.toLocaleLowerCase().includes(this.state.searchInput.toLocaleLowerCase()));
     return (
       <main className="ContactContainer">
-        <CotanctContainerHeder />
-        <ContactContainerBody contacts={this.state.contacts} />
+        <input
+          onChange={this.searchInputHandler}
+          type="text"
+          name="searchInput"
+          placeholder="Looking for someone..."
+          value={this.state.searchInput}
+        />
+        <ContactContainerBody contacts={contacts} />
       </main>
     );
   }
